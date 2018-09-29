72c9a979d6f554e868ebbe1d3732351a329ea05b
Eamon Walsh <ewalsh@termysequence.com>
2018-09-29 00:16:37 -0400

    Debian packaging

--- bin/debian-qt.sh
+++ bin/debian-qt.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -d ubuntu.qt ]; then
+if [ ! -d debian.qt ]; then
     echo 'Run me from the termy-packaging directory' 1>&2
     exit 1
 fi
@@ -10,8 +10,8 @@ fi
 phome=$(pwd)
 ident=$(git config user.email)
 
-vers=$(perl -ne 'm/\((.*)-.*\)/; print $1; exit' ubuntu.qt/changelog)
-rel=$(perl -ne 'm/\(.*-(.*)\)/; print $1; exit' ubuntu.qt/changelog)
+vers=$(perl -ne 'm/\((.*)-.*\)/; print $1; exit' debian.qt/changelog)
+rel=$(perl -ne 'm/\(.*-(.*)\)/; print $1; exit' debian.qt/changelog)
 
 mkdir -p ~/deb
 cd ~/deb
@@ -22,7 +22,7 @@ ln -sf termysequence-qt-${vers}.tar.xz termysequence-qt_${vers}.orig.tar.xz
 rm -rf termysequence-${vers}/
 tar Jxf termysequence-qt-${vers}.tar.xz
 
-cp -r $phome/ubuntu.qt termysequence-${vers}/debian
+cp -rL $phome/debian.qt termysequence-${vers}/debian
 
 cd termysequence-${vers}/
 debuild -S -k${ident}
@@ -31,10 +31,4 @@ read -p 'build? ' build
 if [ "$build" = 'y' ]; then
     debuild -us -uc
     exit
-fi
-
-read -p 'upload? ' upload
-if [ "$upload" = 'y' ]; then
-    cd ~/deb
-    dput ppa:sigalrm/termysequence termysequence-qt_${vers}-${rel}_source.changes
 fi
--- bin/debian-server.sh
+++ bin/debian-server.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -d ubuntu.server ]; then
+if [ ! -d debian.server ]; then
     echo 'Run me from the termy-packaging directory' 1>&2
     exit 1
 fi
@@ -10,8 +10,8 @@ fi
 phome=$(pwd)
 ident=$(git config user.email)
 
-vers=$(perl -ne 'm/\((.*)-.*\)/; print $1; exit' ubuntu.server/changelog)
-rel=$(perl -ne 'm/\(.*-(.*)\)/; print $1; exit' ubuntu.server/changelog)
+vers=$(perl -ne 'm/\((.*)-.*\)/; print $1; exit' debian.server/changelog)
+rel=$(perl -ne 'm/\(.*-(.*)\)/; print $1; exit' debian.server/changelog)
 
 mkdir -p ~/deb
 cd ~/deb
@@ -22,7 +22,7 @@ ln -sf termysequence-server-${vers}.tar.xz termysequence-server_${vers}.orig.tar
 rm -rf termysequence-${vers}/
 tar Jxf termysequence-server-${vers}.tar.xz
 
-cp -r $phome/ubuntu.server termysequence-${vers}/debian
+cp -rL $phome/debian.server termysequence-${vers}/debian
 
 cd termysequence-${vers}/
 debuild -S -k${ident}
@@ -31,10 +31,4 @@ read -p 'build? ' build
 if [ "$build" = 'y' ]; then
     debuild -us -uc
     exit
-fi
-
-read -p 'upload? ' upload
-if [ "$upload" = 'y' ]; then
-    cd ~/deb
-    dput ppa:sigalrm/termysequence termysequence-server_${vers}-${rel}_source.changes
 fi
